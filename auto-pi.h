57942d07c52fe480d446821bc6d3b68672732006
Ansel <aesthapus@gmail.com>
2015-07-07 13:08:48 -0400

    Redid indentation to use tabs because the module previously mixed tabs and spaces in a rather hard-to-read manner

--- auto-pi.h
+++ auto-pi.h
@@ -16,86 +16,65 @@
 */
 
 /*
-=========================================================================
-Module Name: AutoPi
+	Created by: Oscar Miranda-Dominguez
 
-Module Version: v1.0
-
-Date: 2011-10-07
-
-Created by: Oscar Miranda-Dominguez
-
-Description: This module uses a PI process to inject current to a cell, it also does auto calculation of the best parameters per cell.
-
-Analysis File: none
-
-References: none 
-
-Notes:
-
-Changelog:
-	-v1.0-Initial version of the module.
-
-=========================================================================
+	Description: This module uses a PI process to inject current to a cell, 
+	it also does auto calculation of the best parameters per cell.
 */
 
  
 #include <default_gui_model.h>
-//#include "AutoPi_gui_model.h"
 
 class AutoPi : public DefaultGUIModel
 {
 
-public:
+	public:
+		AutoPi(void);
+		virtual ~AutoPi(void);
 
-   AutoPi(void);
-    virtual ~AutoPi(void);
+		// the main function run every time step, contains model logic
+		virtual void execute(void);
 
-    // the main function run every time step, contains model logic
-    virtual void execute(void);
+	protected:
+		// run each time model parameters are updated
+		virtual void update(DefaultGUIModel::update_flags_t);
 
-protected:
-
-    // run each time model parameters are updated
-    virtual void update(DefaultGUIModel::update_flags_t);
-
-private:
-
-        // internal variables
-    double kp;
-    double ti;
-    double td;
-    double e;         //e is the error
-    double pe;        //pe is the previous error
-    double target;
-    double P;
-    double I;
-    double D;
-    double dt;
-    double t;
-    double tt;//time threshold
-    double CurrentState;
-    double Last_t_spike;
-    double Current_t_spike;
-    double OldConstantCurrent;
-    double ConstantCurrent;
-    double t_spike;
-    double dI;
-    double increase;//porcentage to increase (default 20%)
-    double ISIs [20];
-    double mean_ISI;
-    double last_ISI;
-    double min_ISI;
-    double delta_y;
-    double delta_x;
-    double K;
-    double tau;
-    double a;
-    long long count;
-    int state;
-    int counter;
-    int autotune;
-    int stage;
-    int first;
-    int HoldOn;
+	private:
+		// internal variables
+		double kp;
+		double ti;
+		double td;
+		double e;         //e is the error
+		double pe;        //pe is the previous error
+		double target;
+		double P;
+		double I;
+		double D;
+		double dt;
+		double t;
+		double tt;//time threshold
+		double CurrentState;
+		double Last_t_spike;
+		double Current_t_spike;
+		double OldConstantCurrent;
+		double ConstantCurrent;
+		double t_spike;
+		double dI;
+		double increase;//porcentage to increase (default 20%)
+		double ISIs [20];
+		double mean_ISI;
+		double last_ISI;
+		double min_ISI;
+		double delta_y;
+		double delta_x;
+		double K;
+		double tau;
+		double a;
+		long long count;
+		int state;
+		int counter;
+		int autotune;
+		int stage;
+		int first;
+		int HoldOn;
 };
